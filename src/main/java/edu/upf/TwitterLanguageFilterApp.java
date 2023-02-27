db7274e63875062632a9cae0f6e65a289f56ef6e
SergiHernandez <47487884+SergiHernandez@users.noreply.github.com>
2023-02-27 17:52:56 +0100

    Removed unnecessary flatMap

--- src/main/java/edu/upf/TwitterLanguageFilterApp.java
+++ src/main/java/edu/upf/TwitterLanguageFilterApp.java
@@ -3,11 +3,9 @@ package edu.upf;
 import edu.upf.model.SimplifiedTweet;
 //import edu.upf.uploader.S3Uploader;
 
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
-import scala.Tuple2;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -32,13 +30,8 @@ public class TwitterLanguageFilterApp {
         
         long TweeeetsCount = tweets.count();
         System.out.println("\n\nTotal number of tweets: " + TweeeetsCount + "\n\n"); // Debugging
-        //JavaRDD<String> filteredTweets = tweets.filter(tweet -> );
-        //SimplifiedTweet(tweetId, text, userId, userName, language, timestampMs)
-        
-        JavaRDD<String> filteredTweets = tweets
-                                        .flatMap(tweet_page -> Arrays.asList(tweet_page.split("\\{\\}")).iterator());//Read raw tweet (line)
 
-        JavaRDD<SimplifiedTweet> simplifiedTweets = filteredTweets
+        JavaRDD<SimplifiedTweet> simplifiedTweets = tweets
                                         .map(tweet -> SimplifiedTweet.fromJson(tweet))//convert raw tweet to SimplifiedTweet with optional type
                                         .filter(tweet -> tweet.isPresent())//Check that the Optional SimplifiedTweet is not empty
                                         .map(tweet -> tweet.get())
@@ -49,11 +42,7 @@ public class TwitterLanguageFilterApp {
         System.out.println("\n\nNumber of tweets: " + TweetsCount + "\n\n");
         simplifiedTweets.saveAsTextFile(outputFile);
 
-        //final S3Uploader uploader = new S3Uploader(bucket, language); //The prefix is the language
-        //uploader.upload(Arrays.asList(filteredTweets));
-
         long stop = System.currentTimeMillis();
         System.out.println("Done in ms: " + (stop - start));
-        sc.close();
     }
 }
